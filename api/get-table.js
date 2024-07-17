b151e43874e599c424ea41291080878bcd38b163
asadd2723 <asadu3040@gmail.com>
2024-07-18 03:29:15 +0500

    post

--- api/get-table.js
+++ api/get-table.js
@@ -2,8 +2,8 @@ import { sql } from "@vercel/postgres";
 
 export default async function(req, res){
   try {
-    const result = await sql `SELECT * FROM asad`
-    return res.json(result)
+    await sql `DROP TABLE IF EXISTS asad;`
+    return console.log('done')
   } catch (error) {
     return res.error
   }
